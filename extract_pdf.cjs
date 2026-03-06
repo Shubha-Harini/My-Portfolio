93a8e60a51ca189178742a713d19898efa5eed38
Shubha-Harini <harinishanmugam76@gmail.com>
2026-03-06 16:06:32 +0530

    Fix 404 assets errors by using relative base path and cleanup temporary scripts

--- extract_pdf.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const pdf = require('pdf-parse');
-let dataBuffer = fs.readFileSync('public/Shubha Harini S Resume.pdf');
-pdf(dataBuffer).then(function (data) {
-  console.log(data.text);
-}).catch(console.error);
